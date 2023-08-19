ef6f7c35599b63edf5c2f3d5579f261b1650784c
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-08-19 17:09:55 +0200

    finished build-web-api-minimal-spa

---
+++ aspnet-core-minimal-api/build-web-api-minimal-spa/pizza-web/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PizzaMain from './pizza-main.jsx';
+
+function App() {
+  return (
+    <div className='App'>
+      <PizzaMain />
+    </div>
+  )
+}
+
+export default App
